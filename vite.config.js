e20ab3564e1b6b497b9e19a61b59988e9661376c
danielgazit <daniel.gazit@island.io>
2026-01-11 12:13:30 +0100

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/',
@@ -8,11 +7,6 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-      },
-    },
   },
   server: {
     port: 3000,
